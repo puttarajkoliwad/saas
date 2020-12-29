f3c64ebb5e74069287e192d41768b337b1cccfb2
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-12-29 12:47:19 +0530

    ActionMailer setup for GMAIL-SMTP

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+	:address => "smtp.gmail.com",
+	:port => 587,
+	:authentication => :plain,
+	:domain => ENV['GMAIL_USERNAME'],
+	:user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :enable_starttls_auto => true
+}
